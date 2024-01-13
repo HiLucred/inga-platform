424395bbd005895016105e9b367d9835d288fc36
Gabriel <lucred@protonmail.com>
2024-01-13 18:33:05 -0300

    add: Swiper lib

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,19 @@
+import './globals.css'
+// Swiper config
+import 'swiper/css'
+import 'swiper/css/bundle'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+import { register } from 'swiper/element/bundle'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 import { NavBar } from '@/components/layout/nav-bar'
 import { Footer } from '@/components/layout/footer'
 
 const inter = Inter({ subsets: ['latin'] })
+// Swiper
+register()
 
 export const metadata: Metadata = {
   title: 'Inga',
--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,17 +1,22 @@
+'use client'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
 import { GameCard } from './card/game-card'
 
 export function Carousel() {
   return (
-    <div className="flex w-full items-center gap-2 overflow-hidden rounded-md border border-zinc-800 bg-white bg-opacity-5 p-2">
+    <Swiper
+      slidesPerView={'auto'}
+      spaceBetween={10}
+      className="p rounded-md border border-zinc-800 bg-white bg-opacity-5 p-2"
+    >
       {Array.from({ length: 16 }).map((_, index) => {
         return (
-          <GameCard
-            key={index}
-            title="Vestígios de um domingo incógnito"
-            rating={3}
-          />
+          <SwiperSlide key={index} className="max-w-[9rem]">
+            <GameCard title="Vestígios de um domingo incógnito" rating={3} />
+          </SwiperSlide>
         )
       })}
-    </div>
+    </Swiper>
   )
 }
